79a65ce52f158222a04a704b64d41c808a8e6f94
DerekHs <hsiaoderek@berkeley.edu>
2019-03-24 11:51:07 -0700

    Updates

---
+++ precache-manifest.7319e638480592fb7dfe5c3e79984d49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1b573f72b09086f9eced",
+    "url": "/tip/static/js/runtime~main.a74dde57.js"
+  },
+  {
+    "revision": "6e48574765cbea4259c7",
+    "url": "/tip/static/js/main.a524420b.chunk.js"
+  },
+  {
+    "revision": "0fc5b2599657c5c33c3c",
+    "url": "/tip/static/js/2.53de9f8f.chunk.js"
+  },
+  {
+    "revision": "6e48574765cbea4259c7",
+    "url": "/tip/static/css/main.17ad7f54.chunk.css"
+  },
+  {
+    "revision": "0fc5b2599657c5c33c3c",
+    "url": "/tip/static/css/2.da230abc.chunk.css"
+  },
+  {
+    "revision": "254156d3203cf8421dbe86f8a88cd7da",
+    "url": "/tip/index.html"
+  }
+];
